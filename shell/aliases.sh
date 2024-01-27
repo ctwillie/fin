95395880fcfafad8e2fca4b7b7385d86af84ca57
Cedric Twillie <ctwillie77@gmail.com>
2024-01-26 20:35:28 -0500

    feat: update shell/aliases

---
+++ shell/aliases.sh
@@ -0,0 +1,17 @@
+echo "  ______ _       "
+echo " |  ____(_)      "
+echo " | |__   _ _ __  "
+echo " |  __| | | '_ \ "
+echo " | |    | | | | |"
+echo " |_|    |_|_| |_|"
+                 
+echo "Loading aliases..."
+
+# Common
+alias c="clear"
+alias ll='ls -la'
+
+alias fin:help='echo -e "\n\nc='\''clear'\''\nll='\''ls -la'\'' \n\n"'
+
+echo "Aliases loaded!"
+echo "Run 'fin:help' to see available aliases"
