0723b9916139b1d66e0a85fa60297ea1af3773fc
Cedric Twillie <ctwillie77@gmail.com>
2024-01-26 20:49:34 -0500

    feat: update shell/aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,3 +1,15 @@
+# Author: Cedric Twillie
+# Description:
+#    This script will register aliases into your shell session.
+#
+# Usage: 
+#    To register the aliases into your shell, run the following command:
+#    source <(curl -s https://raw.githubusercontent.com/ctwillie/fin/main/shell/aliases.sh)
+#
+#    To register the aliases into your shell without caching, run the following command:
+#    source <(curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/ctwillie/fin/main/shell/aliases.sh)
+#
+
 echo "  ______ _       "
 echo " |  ____(_)      "
 echo " | |__   _ _ __  "
@@ -5,13 +17,12 @@ echo " |  __| | | '_ \ "
 echo " | |    | | | | |"
 echo " |_|    |_|_| |_|"
                  
-echo -e "\nLoading aliases..."
+echo -e "\nRegistering aliases..."
 
 # Common
 alias c="clear"
 alias ll='ls -la'
 
 alias fin:help='echo -e "c='\''clear'\''\nll='\''ls -la'\''"'
-
 echo "Done!"
-echo -e "\nRun 'fin:help' to see available aliases"
+echo -e "\nRun 'fin:help' to see all registered aliases"
